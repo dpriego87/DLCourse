98897ae5cdb435bce11e7ebfb0b170dbe913d7f5
Daniel Priego-Espinosa <dpriego87@gmail.com>
2018-10-06 13:13:43 -0500

    a

--- week6/LSTM.py
+++ week6/LSTM.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-import numpy
+import numpy as np
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
@@ -42,6 +42,15 @@ X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 print(X_train[0])
 print(y_train[:10])
 
+print(X_train.shape)
+print(y_test.shape)
+
+X_train = np.reshape(X_train,(len(X_train),1,max_review_length))
+X_test = np.reshape(X_test,(len(X_test),1,max_review_length))
+
+
+print(X_train.shape)
+print(X_test.shape)
 
 # In[6]:
 
@@ -49,8 +58,8 @@ print(y_train[:10])
 # create the model
 embedding_vecor_length = 32
 model = Sequential()
-model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-model.add(LSTM(10))
+# model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+model.add(LSTM(10,input_shape=(1,max_review_length)))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
@@ -112,7 +121,3 @@ print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
 # In[ ]:
-
-
-
-
